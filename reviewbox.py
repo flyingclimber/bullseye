2b710015c62a0d3c115aff51593a2b4996e3bf0b
Tomasz Finc <tomasz@gmail.com>
2015-10-21 23:20:30 -0700

    Adding dry run option

--- reviewbox.py
+++ reviewbox.py
@@ -11,12 +11,15 @@ PARSER = argparse.ArgumentParser(description='Given a classifier tag an image')
 PARSER.add_argument('filename', metavar='filename', type=str, nargs='*',
                    help='image to process')
 PARSER.add_argument('-s', type=float, default=0.5, help='scaling factor')
+PARSER.add_argument('-n', action="store_true",
+                    help='dry run, only output changed files')
 
 ARGS = PARSER.parse_args()
 
 CASCADE = 'training/data/cascade.xml'
 IMAGE_LIST = ARGS.filename
 SCALING = ARGS.s
+DRYRUN = ARGS.n
 
 CLASSIFIER = cv2.CascadeClassifier(CASCADE)
 
@@ -25,11 +28,14 @@ for image in IMAGE_LIST:
     GRAY = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
     MATCH = CLASSIFIER.detectMultiScale(GRAY, 1.3, 5)
 
-    for (x, y, w, h) in MATCH:
-        cv2.rectangle(IMG, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    if ARGS.n:
+        print "{} {}".format(image, len(MATCH))
+    else:
+        for (x, y, w, h) in MATCH:
+            cv2.rectangle(IMG, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    RES = cv2.resize(IMG, None, fx=SCALING, fy=SCALING,
-                     interpolation=cv2.INTER_CUBIC)
-    cv2.imshow('Sample', RES)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        RES = cv2.resize(IMG, None, fx=SCALING, fy=SCALING,
+                         interpolation=cv2.INTER_CUBIC)
+        cv2.imshow('Sample', RES)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
