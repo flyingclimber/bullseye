ed784dd5911d7eac27ad846c7eb5ee577689206b
Tomasz Finc <tomasz@gmail.com>
2015-10-21 23:06:13 -0700

    Adding ability to iterate through list of images

--- reviewbox.py
+++ reviewbox.py
@@ -8,27 +8,28 @@ import cv2
 import argparse
 
 PARSER = argparse.ArgumentParser(description='Given a classifier tag an image')
-PARSER.add_argument('filename', metavar='filename', type=str,
+PARSER.add_argument('filename', metavar='filename', type=str, nargs='*',
                    help='image to process')
 PARSER.add_argument('-s', type=float, default=0.5, help='scaling factor')
 
 ARGS = PARSER.parse_args()
 
 CASCADE = 'training/data/cascade.xml'
-SRC_IMAGE = ARGS.filename
+IMAGE_LIST = ARGS.filename
 SCALING = ARGS.s
 
 CLASSIFIER = cv2.CascadeClassifier(CASCADE)
 
-IMG = cv2.imread(SRC_IMAGE)
-GRAY = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
-MATCH = CLASSIFIER.detectMultiScale(GRAY, 1.3, 5)
+for image in IMAGE_LIST:
+    IMG = cv2.imread(image)
+    GRAY = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
+    MATCH = CLASSIFIER.detectMultiScale(GRAY, 1.3, 5)
 
-for (x, y, w, h) in MATCH:
-    cv2.rectangle(IMG, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    for (x, y, w, h) in MATCH:
+        cv2.rectangle(IMG, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-RES = cv2.resize(IMG, None, fx=SCALING, fy=SCALING,
-                 interpolation=cv2.INTER_CUBIC)
-cv2.imshow('Sample', RES)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    RES = cv2.resize(IMG, None, fx=SCALING, fy=SCALING,
+                     interpolation=cv2.INTER_CUBIC)
+    cv2.imshow('Sample', RES)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
