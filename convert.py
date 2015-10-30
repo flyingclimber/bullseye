1c084ac4c31ac8619c35a1b3cff797c1b6648240
Tomasz Finc <tomasz@gmail.com>
2015-10-29 23:13:14 -0700

    Adding cbr and cbz support

--- convert.py
+++ convert.py
@@ -1,22 +1,35 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 
 from wand.image import Image
 import argparse
+import os
+import rarfile
+import zipfile
 
 RESOLUTION = 300
 
 PARSER = argparse.ArgumentParser(description='Convert pdf to jpg')
-PARSER.add_argument('-f', type=str, help="pdf to read")
+PARSER.add_argument('filename', metavar='filename', type=str,
+                   help='image to process')
 PARSER.add_argument('-o', type=str, help="output file")
 PARSER.add_argument('-i', type=int, default=RESOLUTION, help="resolution")
 
 ARGS = PARSER.parse_args()
 
-INPUT_FILE = ARGS.f
+INPUT_FILE, INPUT_FILE_EXTENSION = os.path.splitext(ARGS.filename)
 OUTPUT_FILE = ARGS.o
 
-FILE_NAME = '{}'.format(INPUT_FILE)
+FILE_NAME = '{}'.format(INPUT_FILE + INPUT_FILE_EXTENSION)
 
-with Image(filename=FILE_NAME, resolution=RESOLUTION) as pdf:
-    print len(pdf.sequence)
-    pdf.save(filename=OUTPUT_FILE)
+if INPUT_FILE_EXTENSION == '.pdf':
+    with Image(filename=FILE_NAME, resolution=RESOLUTION) as pdf:
+        print len(pdf.sequence)
+        pdf.save(filename=OUTPUT_FILE)
+else:
+    if INPUT_FILE_EXTENSION == '.cbr':
+        ARCHIVE = rarfile.RarFile(FILE_NAME)
+
+    elif INPUT_FILE_EXTENSION == '.cbz':
+        ARCHIVE = zipfile.ZipFile(FILE_NAME)
+
+    ARCHIVE.extractall()
